cdbbc6de7a4c3aa2293394410a95c9ee6b9f8f24
Farizerz <39519098+Farizerz@users.noreply.github.com>
2022-05-13 14:37:54 +0700

    Finished resistance and diode, working on Continuity

--- FA/Operating Measurements/Diode/Diode.cs
+++ FA/Operating Measurements/Diode/Diode.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class Resistance : MonoBehaviour
+public class Diode : MonoBehaviour
 {
     [Header("Game Object")]
     public GameObject RotarySwitch;
@@ -16,6 +16,7 @@ public class Resistance : MonoBehaviour
     public GameObject[] HoldIndicator;
     public GameObject RedPen;
     public GameObject BlackPen;
+    public GameObject DiodeObject;
 
     [Header("UI")]
     public GameObject[] RotarySwitchUI;
@@ -28,7 +29,9 @@ public class Resistance : MonoBehaviour
     public TextMeshProUGUI[] VoltText;
     public GameObject[] HoldButton;
     public GameObject[] HoldText;    
-    public GameObject [] DisplayText;
+    public GameObject[] DisplayText;
+    public GameObject ForwardBiasUI;
+    public GameObject ReverseBiasUI;
 
     [Header("Modifier")]
     public static bool isDragging;
@@ -39,12 +42,13 @@ public class Resistance : MonoBehaviour
     bool isBothConnected;
     bool isCorrectPosition;
     bool isOn;
+    bool isCorrectBias;
     public string scene;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        isCorrectBias = false;
     }
 
     // Update is called once per frame
@@ -75,17 +79,18 @@ public class Resistance : MonoBehaviour
             
         }
 
-        //switch to V-
+        //switch to Diode
         if(switching) { 
             switchingTime -= Time.deltaTime;
-            if(RotarySwitch.transform.localEulerAngles.z < 240) {
+            if(RotarySwitch.transform.localEulerAngles.z > 60) {
                 RotarySwitch.transform.Rotate(0, 0, switchingTime);
+                Debug.Log(RotarySwitch.transform.localEulerAngles.z);
             } else {
                 isOn = true;
                 switching = false;
                 SocketOn.SetActive(false);
                 SocketOff.SetActive(true);
-                RotarySwitch.transform.localEulerAngles = new Vector3(0, 0, 0);
+                RotarySwitch.transform.localEulerAngles = new Vector3(0, 0, 60);
                 switchingTime = 0;
                 RotarySwitchUI[0].SetActive(false);
                 RotarySwitchUI[1].SetActive(true);
@@ -95,13 +100,13 @@ public class Resistance : MonoBehaviour
         //switch back to OFF
         if(switchingBack) { 
             switchingBackTime += Time.deltaTime;
-            if(RotarySwitch.transform.localEulerAngles.z < 45) {
+            if(RotarySwitch.transform.localEulerAngles.z < 90) {
                 RotarySwitch.transform.Rotate(0, 0, switchingBackTime);
             } else {
                 switchingBack = false;
                 SocketOn.SetActive(true);
                 SocketOff.SetActive(false);
-                RotarySwitch.transform.localEulerAngles = new Vector3(0, 0, 45);
+                RotarySwitch.transform.localEulerAngles = new Vector3(0, 0, 90);
                 switchingBackTime = 0;
                 RotarySwitchUI[1].SetActive(false);
                 RotarySwitchUI[0].SetActive(true);
@@ -109,34 +114,45 @@ public class Resistance : MonoBehaviour
         }        
 
         //check if both pens connected in the correct Position
-        if(ColliderPositif.isPositifConnected && ColliderNegatif.isNegatifConnected && !isHold && isOn) {
+        if(ColliderPositif.isPositifConnected && ColliderNegatif.isNegatifConnected && !isHold && isOn && isCorrectPosition) {
             decrement = increment;
             if(increment <= Volt) {
                 increment+=(Time.deltaTime * speed);
                 var incrementInt = (int) increment;
                     if(incrementInt <= 9) {
-                        VoltText[0].text = "0.0" + incrementInt.ToString();
-                        VoltText[1].text = "0.0" + incrementInt.ToString();
-                    } else {
+                        VoltText[0].text = "0.00" + incrementInt.ToString();
+                        VoltText[1].text = "0.00" + incrementInt.ToString();
+                    } else if (incrementInt >= 10 && incrementInt <= 99) {
+                        VoltText[0].text = "0.0"+ incrementInt.ToString();
+                        VoltText[1].text = "0.0" + incrementInt.ToString();                    
+                    } else if (incrementInt >= 100) {
                         VoltText[0].text = "0."+ incrementInt.ToString();
                         VoltText[1].text = "0." + incrementInt.ToString();                    
-                    }
+                    }                    
 
             }
+        }
+
+        //if the bias is reversed
+        if(!isCorrectBias) {
+            VoltText[0].text = "0.000";
+            VoltText[1].text = "0.000";
+            decrement = 0;
+            increment = 0;            
         } 
 
         //check if one of the pens is released
         if((!ColliderPositif.isPositifConnected || !ColliderNegatif.isNegatifConnected) && !isHold && isOn) {
-            VoltText[0].text = "0.00";
-            VoltText[1].text = "0.00";
+            VoltText[0].text = "0.000";
+            VoltText[1].text = "0.000";
             decrement = 0;
             increment = 0;
         }
 
         //check if the socket is turned off
         if(!isOn && !isHold) {
-            VoltText[0].text = "0.00";
-            VoltText[1].text = "0.00";
+            VoltText[0].text = "0.000";
+            VoltText[1].text = "0.000";
             decrement = 0;
             increment = 0;
         }        
@@ -153,16 +169,16 @@ public class Resistance : MonoBehaviour
 
     public void changeMode() {
         switching = true;
-        Volt += 98;
+        Volt += 679;
         DisplayText[0].SetActive(true);
         DisplayText[1].SetActive(true);
     }
     public void changeModeBack() {
         switchingBack = true;
-        Volt -= 98;
+        Volt -= 679;
         isOn = false;
-        VoltText[0].text = "0.00";
-        VoltText[1].text = "0.00";
+        VoltText[0].text = "0.000";
+        VoltText[1].text = "0.000";
         decrement = 0;
         increment = 0;
         DisplayText[0].SetActive(false);
@@ -200,4 +216,18 @@ public class Resistance : MonoBehaviour
         HoldButton[0].SetActive(true);
         HoldButton[1].SetActive(false);
     }  
+
+    public void forwardBias() {
+        isCorrectBias = true;
+        DiodeObject.transform.Rotate(0, 180, 0);
+        ForwardBiasUI.SetActive(false);
+        ReverseBiasUI.SetActive(true);
+    }
+
+    public void reverseBias() {
+        isCorrectBias = false;
+        DiodeObject.transform.Rotate(0, 180, 0);
+        ForwardBiasUI.SetActive(true);
+        ReverseBiasUI.SetActive(false);
+    }    
 }
